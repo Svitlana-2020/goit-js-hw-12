9b52a8cf505892aa8ecdb6380c153e32e688fb36
Svitlana <160290680+Svitlana-2020@users.noreply.github.com>
2024-07-17 11:18:47 +0100

    scroll doesn't work

--- src/main.js
+++ src/main.js
@@ -1,125 +1,114 @@
 // Описаний у документації
-import iziToast from "izitoast";
+import iziToast from 'izitoast';
 // Додатковий імпорт стилів
-import "izitoast/dist/css/iziToast.min.css";
+import 'izitoast/dist/css/iziToast.min.css';
 
-import { getPictureByQuery, params } from "./js/pixabay-api";
-import { lightbox, renderGalleryMarkup } from "./js/render-functions";
-import { refs } from "./js/refs";
-
+import { getPictureByQuery, params } from './js/pixabay-api';
+import { lightbox, renderGalleryMarkup } from './js/render-functions';
+import { refs } from './js/refs';
 
 refs.formForm.addEventListener('submit', handleSearch);
 refs.loadMoreBtn.addEventListener('click', loadMore);
 
-           async function handleSearch(e) {
-                        e.preventDefault();
-                        const form = e.currentTarget;
-                        params.q = form.elements.inputf.value.trim();
-                        refs.loader.classList.remove('hidden');
-                        refs.galleryCard.innerHTML = '';
-                        params.page = 1;
-                        if (params.q === "") {
-                            refs.loader.classList.add('hidden')
-                            {
-                                
-                                iziToast.show({
-                                    message: '"Enter a search query in a search field. Pls try again"',
-                                    messageColor: 'white',
-                                    messageLineHeight: '150%',
-                                    backgroundColor: '#ef4040',
-                                    position: 'topRight'
-                                            })
-                                    }
-                                    return;
-                        }
-                        
-                        refs.loadMoreBtn.classList.add('hidden');
-                        
-                        try {
-                            const dataResponse = await getPictureByQuery(params);
-                            const { hits, totalHits } = dataResponse;
-                            console.log(hits, totalHits);
-
-                            params.maxPage = Math.floor(totalHits / params.per_page);
-                            console.log(params.maxPage);
-                            
-                            
-                            if (hits && hits.length > 0) {
-                                renderGalleryMarkup(hits);
-                                refs.loadMoreBtn.classList.remove('hidden');
-  
-                                                                    
-                            } else {
-                                throw new Error('No images found');
-                            }
-                        } catch (error) {;
-                            fetchError(error);
-                        } finally {
-                            refs.loader.classList.add('hidden')
-                            form.reset(); // Сброс формы даже в случае ошибки
-                        }
-                    }
-
-                    function fetchError(error) {
-                        iziToast.show({
-                            message: '"Sorry, there are no images matching your search query. Please try again!"',
-                            messageColor: 'white',
-                            messageLineHeight: '150%',
-                            backgroundColor: '#ef4040',
-                            position: 'topRight'
-                                    })
-                            }
-
-
-async function loadMore() {
-
-    if (params.maxPage === params.page){
-        refs.loader.classList.add('hidden');
-        iziToast.show({
-            message: "We're sorry, but you've reached the end of search results.",
-            messageColor: 'white',
-            messageLineHeight: '150%',
-            backgroundColor: '#ef4040',
-            position: 'topRight'
-                    })
-return;
+async function handleSearch(e) {
+  e.preventDefault();
+  const form = e.currentTarget;
+  params.q = form.elements.inputf.value.trim();
+  refs.loader.classList.remove('hidden');
+  refs.galleryCard.innerHTML = '';
+  params.page = 1;
+  if (params.q === '') {
+    refs.loader.classList.add('hidden');
+    {
+      iziToast.show({
+        message: '"Enter a search query in a search field. Pls try again"',
+        messageColor: 'white',
+        messageLineHeight: '150%',
+        backgroundColor: '#ef4040',
+        position: 'topRight',
+      });
     }
+    return;
+  }
 
-    refs.loader.classList.remove('hidden');
-    params.page += 1;
-    
+  refs.loadMoreBtn.classList.add('hidden');
 
-    try {
+  try {
+    const dataResponse = await getPictureByQuery(params);
+    const { hits, totalHits } = dataResponse;
+    console.log(hits, totalHits);
 
-        const dataResponse = await getPictureByQuery(params);
-        const { hits } = dataResponse;
-        console.log(hits);
-        
-        if (hits && hits.length > 0) {
-            renderGalleryMarkup(hits);
-            refs.loadMoreBtn.classList.remove('hidden');
-            
+    params.maxPage = Math.floor(totalHits / params.per_page);
+    console.log(params.maxPage);
 
-            const galleryItem = document.querySelector('.gallery-item');
-            const cardHeight = galleryItem.getBoundingClientRect().height;
-            const cardHeightValue = cardHeight * 4;
-        
-  
-        window.scrollBy({
-            top: cardHeightValue,
-            behavior: "smooth",
-        });
-   
+    if (hits && hits.length > 0) {
+      renderGalleryMarkup(hits);
 
-        } else {
-            throw new Error('No images found');
-        }
-    } catch (error) {;
-        fetchError(error);
+      refs.loadMoreBtn.classList.remove('hidden');
+    } else {
+      throw new Error('No images found');
     }
-    finally {
-        refs.loader.classList.add('hidden');
-        // form.reset(); // Сброс формы даже в случае ошибки
-    }
+  } catch (error) {
+    fetchError(error);
+  } finally {
+    refs.loader.classList.add('hidden');
+    form.reset(); // Сброс формы даже в случае ошибки
+  }
 }
 
+function fetchError(error) {
+  iziToast.show({
+    message:
+      '"Sorry, there are no images matching your search query. Please try again!"',
+    messageColor: 'white',
+    messageLineHeight: '150%',
+    backgroundColor: '#ef4040',
+    position: 'topRight',
+  });
+}
+
+async function loadMore(e) {
+  if (params.maxPage === params.page) {
+    refs.loader.classList.add('hidden');
+    iziToast.show({
+      message: "We're sorry, but you've reached the end of search results.",
+      messageColor: 'white',
+      messageLineHeight: '150%',
+      backgroundColor: '#ef4040',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  refs.loader.classList.remove('hidden');
+  params.page += 1;
+
+  try {
+    const dataResponse = await getPictureByQuery(params);
+    const { hits } = dataResponse;
+    console.log(hits);
+
+    // if (hits && hits.length > 0) {
+      renderGalleryMarkup(hits);
+
+      const galleryItem = document.querySelector('.gallery-item');
+      const cardHeight = galleryItem.getBoundingClientRect().height;
+      const cardHeightValue = cardHeight * 4;
+
+      window.scrollBy({
+        top: cardHeightValue,
+        behavior: "smooth",
+      });
+
+      refs.loadMoreBtn.classList.remove('hidden');
+
+    // } else {
+    //   throw new Error('No images found');
+    // }
+  } catch (error) {
+    fetchError(error);
+  } finally {
+    refs.loader.classList.add('hidden');
+    // form.reset(); // Сброс формы даже в случае ошибки
+  }
+}
